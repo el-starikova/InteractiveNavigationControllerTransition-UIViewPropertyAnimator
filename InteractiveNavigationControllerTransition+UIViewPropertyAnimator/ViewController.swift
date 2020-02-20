0125b07105fb74bf7d0433e33c2f4bdfd34eb5d3
starikova <elaine.starikova@gmail.com>
2020-02-20 14:08:27 +0200

    Initial Commit

---
+++ InteractiveNavigationControllerTransition+UIViewPropertyAnimator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InteractiveNavigationControllerTransition+UIViewPropertyAnimator
+//
+//  Created by starikova on 2/20/20.
+//  Copyright © 2020 starikova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
